a22b1a0b2fa07be0991503c548a2d07e32e8afc0
Vinh Nguyen <nguyenbavinhcntt@gmail.com>
2023-09-29 22:52:39 +0700

    Initial api service repository

---
+++ index.js
@@ -0,0 +1,76 @@
+import axios from 'axios';
+axios.interceptors.request.use(function (config) {
+  try {
+    if (typeof localStorage === 'object') {
+      const token = localStorage.getItem('accessToken');
+      if (token && !config.headers.Authorization) {
+        config.headers.Authorization =  'Bearer ' + token;
+      }
+    }
+  } catch(e) {
+  }
+  return config;
+});
+export const APIService = {
+  post: (url, data, cf) => {
+    return axios.post(`${process.env.NEXT_PUBLIC_apiBaseUrl}${url}`,data, cf);
+  },
+  put: (url, data, cf) => {
+    return axios.put(`${process.env.NEXT_PUBLIC_apiBaseUrl}${url}`,data, cf);
+  },
+  get: (url) => {
+    return axios.get(`${process.env.NEXT_PUBLIC_apiBaseUrl}${url}`);
+  },
+  delete : (url, data) => {
+    return axios.delete(`${process.env.NEXT_PUBLIC_apiBaseUrl}${url}`,data);
+  }
+}
+/**
+ * Handle api call error
+ */
+export const handleApiError = (err, { callBackStatusCode, showAlert = true, callBackErrorMessage } = {}) => {
+  let message = 'Unknow error';
+  try {
+    if (typeof callBackStatusCode === 'function') {
+      callBackStatusCode(err.response.status)
+    }
+  } catch (e) { }
+  if (err.response && err.response.data && err.response.data.error) {
+    message = err.response.data.error;
+  }
+  if (typeof callBackErrorMessage === 'function') {
+    callBackErrorMessage(message)
+  }
+
+  if (showAlert) {
+    alert(message);
+  }
+}
+
+export const STATUS_CODE = {
+  DUPLICATE: 409
+}
+/**
+ * Show/hide loader for app
+ * @param {*} dispatch 
+ * @param {*} isLoading 
+ */
+export const setShowLoading = (dispatch, isLoading) => {
+  if (typeof dispatch === 'function') {
+    dispatch({type: 'LOADING', payload: isLoading});
+  }
+}
+
+export const makeQueryParamsFromObject = params => {
+  let result = '', keys = [];
+  if (params && typeof params === 'object') {
+    keys = Object.keys(params);
+    keys = keys.filter(item => ![null, undefined, ''].includes(params[item]));
+  }
+  if (keys.length > 0) {
+    result += '?' + keys.map(item => {
+      return `${item}=${params[item]}`;
+    }).join('&')
+  }
+  return result;
+}
